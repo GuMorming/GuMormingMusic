9f2fbf5ad56754cafeea0b2468971a20ff970bd7
GuMorming <gumorming@163.com>
2024-02-17 18:10:16 +0800

    创建工程,合并模块

---
+++ GuMormingMusic-Admin/src/main/java/cn/edu/whut/gumormingmusic/GuMormingMusicAdminApplication.java
@@ -0,0 +1,19 @@
+package cn.edu.whut.gumormingmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : GuMorming
+ * @Project : GuMormingMusic
+ * @Package : cn.edu.whut.gumormingmusic
+ * @createTime : 2024/2/17 18:05
+ * @Email : gumorming@163.com
+ * @Description :
+ */
+@SpringBootApplication
+public class GuMormingMusicAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GuMormingMusicAdminApplication.class, args);
+    }
+}
